90b7e03a518f0265ffc138a8f94aa4e7ffb64042
unknown <Manasvi-1612>
2024-02-10 09:45:49 +0530

    seventh commit

--- components/shared/About.tsx
+++ components/shared/About.tsx
@@ -1,12 +1,36 @@
+import Link from 'next/link';
+import ButtonComponent from './ButtonComponent';
 import PrimeTitle from './PrimeTitle';
 
 const About = () => {
+
+    const openResume = () => {
+        window.open('file:///C:/Users/hp/Downloads/ManasviJoshiResume%20(1).pdf', '_blank'); // Change the path to your resume file
+    }
+
     return (
         <>
             <section className='flex items-center justify-center'>
-                <div>
-                    <PrimeTitle title="Resume" subtitle="About" span="me" />
+                <PrimeTitle title="Resume" subtitle="About" span="me" />
+            </section>
+            <section className='flex items-center justify-center'>
+                <div className='text-title-light dark:text-white my-5'>
+                    <h1 className="uppercase h5-bold py-5">Personal Info</h1>
+                    <div className='grid grid-cols-2 gap-2 pb-10'>
+                        <div>First Name: <span className='p-medium-16'>Manasvi</span></div>
+                        <div>Last Name: <span className='p-medium-16'>Joshi</span></div>
+                        <div>Address: <span className='p-medium-16'>Jaipur, Rajasthan</span></div>
+                        <div>Email: <span className='p-medium-16'>manasvijoshi63@gmail.com</span></div>
+                        <div>Phone: <span className='p-medium-16'>+91 8290679916</span> </div>
+                        <div>Nationality: <span className='p-medium-16'>Indian</span> </div>
+                    </div>
+
+                    {/* <Link  href={'public/images/table_data.pdf'}  target="_blank"  rel="noopener noreferrer" locale={false} download>Download Here</Link> */}
+                    <Link href='/ManasviJoshiResume.pdf' target="_blank"  rel="noopener noreferrer" locale={false}  >
+                        <ButtonComponent title='Download cv' name='Download' />
+                    </Link>
                 </div>
+
             </section>
         </>
     )
--- components/shared/PrimeTitle.tsx
+++ components/shared/PrimeTitle.tsx
@@ -8,10 +8,12 @@ const PrimeTitle = ({ title, subtitle, span }: PrimeTitleProps) => {
   return (
     //  
     <>
-      <div className="my-20 uppercase fixed right-0 left-0 text-center -z-10">
-        <h1 className='prime-title'>{title}</h1>
-        <h2 className="h2-bold z-30 text-title-light dark:text-white m-0">{subtitle} <span className="text-primary-light dark:text-primary-dark">{span}</span></h2>
-      </div>
+      <section className="my-20">
+        <div className="uppercase fixed right-0 left-0 text-center -z-10">
+          <h1 className='prime-title'>{title}</h1>
+          <h2 className="h2-bold z-30 text-title-light dark:text-white m-0">{subtitle} <span className="text-primary-light dark:text-primary-dark">{span}</span></h2>
+        </div>
+      </section>
     </>
   )
 }
