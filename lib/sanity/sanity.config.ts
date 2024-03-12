752bd4d54d3812e4a4c8a4b0a1aca8ad4a944fce
unknown <Manasvi-1612>
2024-03-13 00:48:01 +0530

    fix-sanity

--- lib/sanity/sanity.config.ts
+++ lib/sanity/sanity.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity/lib/exports'
+import { structureTool } from 'sanity/lib/exports/structure'
+import { visionTool } from './node_modules/@sanity/vision'
 import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
